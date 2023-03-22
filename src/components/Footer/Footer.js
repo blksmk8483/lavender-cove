25961ae027c22b66090e23145323174b791a47de
Brandon Knight <knightbrandon1984@gmail.com>
2023-03-22 14:08:43 -0500

    added the links to the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,20 +3,28 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import EmailIcon from '@mui/icons-material/Email';
 import CallIcon from '@mui/icons-material/Call';
-import './Footer.css'
+import './Footer.css';
 
 
 function Footer() {
     return (
-    <div className="footer">
-        <div className="socialMedia"> 
-            <InstagramIcon />
-            <FacebookIcon />
-            <EmailIcon />
-            <CallIcon />
+        <div className="footer">
+            <div className="socialMedia">
+                <a target="_blank" rel="nonreferrer noreferrer" href="https://www.instagram.com/lavendercovefarm/">
+                    <InstagramIcon />
+                </a>
+                <a  target="_blank" rel="nonreferrer noreferrer" href="https://www.facebook.com/lavendercovefarm">
+                <FacebookIcon />
+                </a>
+                <a  target="_blank" rel="nonreferrer noreferrer" href="mailto: contact@lavendercovefarm.com">
+                <EmailIcon />
+                </a>
+                <a  target="_blank" rel="nonreferrer noreferrer" href="tel:2546130264">
+                <CallIcon />
+                </a>
+            </div>
+            <p> &copy; 2023 Brandon Knight </p>
         </div>
-        <p> &copy; 2023 Brandon Knight </p>
-    </div>
     );
 }
 
